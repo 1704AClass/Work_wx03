23ad5f9b38052125905f1401301ce68c6145f9bb
WX <786587792@qq.com>
2020-03-17 11:53:17 +0800

    3月17

--- code/nm-framework-parent/nm-service-manage-course/src/main/java/com/ningmeng/manage_course/dao/CourseMarketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ningmeng.manage_course.dao;
-
-import com.ningmeng.framework.domain.course.CourseMarket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseMarketRepository extends JpaRepository<CourseMarket,String> {
-}
--- code/nm-framework-parent/nm-service-manage-media/src/main/java/com/ningmeng/manage_media/dao/MediaFileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ningmeng.manage_media.dao;
-
-import com.ningmeng.framework.domain.media.MediaFile;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface MediaFileRepository extends MongoRepository<MediaFile,String> {
-}
